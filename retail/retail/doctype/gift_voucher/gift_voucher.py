f459b06742d81273a3a67a2868ac998354071cd8
iamtalib13 <talibsheikh16@gmail.com>
2025-03-09 12:50:44 +0530

    feat: Assign gift voucher name as serial number and link to Serial No

--- retail/retail/doctype/gift_voucher/gift_voucher.py
+++ retail/retail/doctype/gift_voucher/gift_voucher.py
@@ -1,9 +1,22 @@
-# Copyright (c) 2025, Talib Sheikh and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe.utils import get_link_to_form
 
 class GiftVoucher(Document):
-	pass
+    def on_submit(self):
+        """Set voucher name as serial number if not already set."""
+        if self.gift_voucher_code:
+            return
+        if not frappe.db.exists("Serial No", self.name):
+            frappe.get_doc({
+                "doctype": "Serial No",
+                "serial_no": self.name,
+                "item_code": "Gift Voucher"
+            }).insert(ignore_permissions=True)
+        self.db_set("gift_voucher_code", self.name)
+        frappe.msgprint(
+            f"<b>Gift Voucher Created Successfully!</b><br>"
+            f"Serial Number: {get_link_to_form('Serial No', self.name)}",
+            title="Success",
+            indicator="green"
+        )
