1a1e0f6bc26f48ffb0c01bd532b74a12922a95ef
iamtalib13 <talibsheikh16@gmail.com>
2025-03-13 21:35:12 +0530

    feat: added custom fields for sales invoice payment child table to capture Active gift vouchers details

--- retail/patches/custom_fields/add_custom_fields_sales_invoice.py
+++ retail/patches/custom_fields/add_custom_fields_sales_invoice.py
@@ -27,7 +27,41 @@ def execute():
                 "fetch_from": "custom_gift_voucher_code.initial_balance",
                 "insert_after": "custom_gift_voucher_code",
                 "label": "Gift Voucher Amount",
+                "no_copy": 1,
+                "read_only": 1,
+                "depends_on": "eval:doc.custom_gift_voucher_code",
+            },
+            
+            
+        ],
+         "Sales Invoice Payment": [
+            {
+                "fieldname": "custom_gift_voucher_details",
+                "fieldtype": "Section Break",
+                "insert_after": "clearance_date",
+                "label": "Gift Voucher Details",
+                "depends_on": "eval:doc.mode_of_payment == \"Gift Voucher\"",
+               
+            },
+            {
+                "fieldname": "custom_gift_voucher_code",
+                "fieldtype": "Link",
+                "insert_after": "custom_gift_voucher_details",
+                "label": "Gift Voucher Code",
+                "options": "Gift Voucher",
+                "mandatory_depends_on": "eval:doc.mode_of_payment == \"Gift Voucher\"",
                 "no_copy": 1
+            },
+             {
+                "fieldname": "custom_gift_voucher_current_balance",
+                "fieldtype": "Currency",
+                "fetch_from": "custom_gift_voucher_code.current_balance",
+                "insert_after": "custom_gift_voucher_code",
+                "label": "Gift Voucher Current Balance",
+                "no_copy": 1,
+                "read_only": 1,
+                "depends_on": "eval:doc.custom_gift_voucher_code",
+                
             },
             
             
